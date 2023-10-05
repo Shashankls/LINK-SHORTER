319d81ca3db5fbbb2a12872525ab2e4741e23752
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-10-05 23:33:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,9 +19,9 @@ import aiohttp
 from pyrogram import Client, filters
 
 API_ID = environ.get('API_ID',"16621664") 
- API_HASH = environ.get('API_HASH',"8b283f2943729318995738b5963f0bcc") 
- BOT_TOKEN = environ.get('BOT_TOKEN',"6598745712:AAHogV8QoYl_xYDAsHCjesktcxYLaHy4IbE") 
- API_KEY = environ.get('API_KEY',"c8ce5ce72bbc4eeea88fa44ce14e06f6e53646b1")
+API_HASH = environ.get('API_HASH',"8b283f2943729318995738b5963f0bcc") 
+BOT_TOKEN = environ.get('BOT_TOKEN',"6598745712:AAHogV8QoYl_xYDAsHCjesktcxYLaHy4IbE") 
+API_KEY = environ.get('API_KEY',"c8ce5ce72bbc4eeea88fa44ce14e06f6e53646b1")
 
 bot = Client('sharedisk.in',
              api_id=API_ID,
